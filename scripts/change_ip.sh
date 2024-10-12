558d48674c940e034e4a9ddc196deb1f7dd88d84
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-10-12 10:18:42 +0300

    Update and rename 1 to change_ip.sh

---
+++ scripts/change_ip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Использование: $0 <IP-адрес>"
+    exit 1
+fi
+
+USER_IP=$1
+mysql -u root -po <<EOF
+USE wordpress;
+UPDATE wp_options SET option_value='https://$USER_IP' WHERE option_name='home';
+UPDATE wp_options SET option_value='https://$USER_IP' WHERE option_name='siteurl';
+EOF
